9e89fd96eea025d2040e72de8f28e8877c68c5f4
Robot <dgoldsb@live.nl>
2016-10-08 19:37:04 +0200

    [Bleep bloop automatic] 0 files added, 1 files altered, 0 files removed. In total there are 4 additions, 2 alterations, 3 deletions.
    testcase_old.py: 
    - Made add in function color
    - Made change in function make_plot
    
    Showerthought of the day:
    If I'm delivering a large sum of money for deposit at the bank, I'm more afraid of the cops taking it from me on my trip there than robbers.

--- testcase_old.py
+++ testcase_old.py
@@ -1,11 +1,14 @@
 import numpy as np
 import matplotlib.plot as plot
 
+def color():
+    return 'g'
+
 def make_plot(x, y):
-    plt.plot(x,y)
+    plt.plot(x,y, color='g')
     plt.show()
 
     return 1
 
 x = np.linspace(0, 10, 1000)
-y = x**2
+y = x**3
