22c833bf9e6488f4193fb572a62f7076abce9f55
dgoldsb <dgoldsb@live.nl>
2016-10-08 19:36:47 +0200

    Restore old.

--- testcase_old.py
+++ testcase_old.py
@@ -1,14 +1,11 @@
 import numpy as np
 import matplotlib.plot as plot
 
-def color():
-    return 'g'
-
 def make_plot(x, y):
-    plt.plot(x,y, color='g')
+    plt.plot(x,y)
     plt.show()
 
     return 1
 
 x = np.linspace(0, 10, 1000)
-y = x**3
+y = x**2
