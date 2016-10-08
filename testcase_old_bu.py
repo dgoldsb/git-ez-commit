6d63e1f5c71ce1a19cc1d7ffd1125a2058bf48f8
Robot <dgoldsb@live.nl>
2016-10-08 20:28:22 +0200

    [Bleep bloop automatic] 0 files added, 2 files altered, 0 files removed. In total there are 10 additions, 4 alterations, 8 deletions.
    testcase_old.py: 
    - Made delete in ClassDef at lineno. 4
    - Made add in class bunny
    - Made change in the main, amongst others in Import
    
    testcase_old_bu.py: 
    - Made delete in ClassDef at lineno. 4
    - Made add in class bunny
    - Made change in the main, amongst others in Import
    
    Showerthought of the day:
    Those who make a big deal out of people sharing 99% of genes with chimps don't mention that people also share 50% of genes with bananas

--- testcase_old_bu.py
+++ testcase_old_bu.py
@@ -1,14 +1,17 @@
 import numpy as np
 import matplotlib.plot as plot
 
-class Naam():
+class bunny():
     pass
 
+def color():
+    return 'g'
+
 def make_plot(x, y):
-    plt.plot(x,y)
+    plt.plot(x,y, color='g')
     plt.show()
 
     return 1
 
 x = np.linspace(0, 10, 1000)
-y = x**2
+y = x**3
